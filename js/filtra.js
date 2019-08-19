5944e111b46d9f93f3a43b74ad1119ff5bc6e04f
marcelommo <marcelomiranda_sp@hotmail.com>
2019-08-19 13:42:26 -0300

    inclusao do campo de filtro da tabela

--- js/filtra.js
+++ js/filtra.js
@@ -1,15 +1,28 @@
 //procurar por cada letra digitada no campo de filtro pelo evento de digitar(Evento Input)
+//esconder tudo que for diferente do value
 
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
-
+  console.log(this.value);
   var pacientes = document.querySelectorAll(".paciente");
 
-  for(var i = 0; i < pacientes.lenght; i++){
-    var paciente = pacientes[i];
-    var tdNome = paciente.querySelector("info-nome");
-    var nome = paciente.textContent;
+  if( this.value.length > 0){
+    for( var i = 0; i < pacientes.length; i++){
+      var paciente = pacientes[i];
+      var tdNome = paciente.querySelector(".info-nome");
+      var nome = tdNome.textContent;
+      if( nome != this.value){
+        paciente.classList.add("invisivel");
+      } else {
+        paciente.classList.remove("invisivel");
+      }
+    }
+  } else {
+      for(var i=0; i < pacientes.length; i++){
+        var paciente = pacientes[i];
+        paciente.classList.remove("invisivel");
+      }
   }
 
 });
